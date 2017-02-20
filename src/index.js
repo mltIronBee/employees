258ffee0f93a4dde3e03f8af38f6cea78528c564
Nikita <nikita.rabayev@do-it.co>
2017-02-20 13:26:11 +0200

    Set up react-router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute,browserHistory } from 'react-router';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+      <Route path="/">
+          <IndexRoute component={App}/>
+      </Route>
+  </Router>,
   document.getElementById('root')
 );
