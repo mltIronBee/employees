423da480068a7ef274c6a38f7b4d2a6a5c30eacf
Nikita <nikita.rabayev@do-it.co>
2017-02-23 13:58:26 +0200

    Config fixes

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  "apiPrefix": "http://" + process.env.API_HOST || 'localhost' + ":" + process.env.API_PORT || 8080,
-  "serverPort": process.env.API_PORT || 8080,
-  "dbConfig": {
-    "name": "test",
-    "host": "localhost",
-    "port": 27017
+  apiPrefix: 'http://' + (process.env.API_HOST || 'localhost') + ':' + (process.env.API_PORT || 8080),
+  serverPort: process.env.API_PORT || 8080,
+  dbConfig: {
+    name: 'test',
+    host: 'localhost',
+    port: 27017
   }
 };
